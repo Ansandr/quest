37d81446149c6b4fb6e7a3ca4b86835519137c13
Ansandr <ansandr2005@gmail.com>
2023-12-01 20:27:32 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+async function fetchRequest() {
+  let password = document.getElementById("password").value;
+  let url = "https://ansandr.pp.ua/quest23/login.php?password=" + password;
+  let response = await fetch(urll, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+  });
+  return await response.json();
+}
+
+function checkPassword() {
+  fetchRequest().then((data) => {
+    if (data.success === 1) {
+      window.location.href = data.url;
+      document.querySelector(".form").remove();
+    }
+  });
+}
