f30dde60f7b59053cf533e41b44baef0f0b5749f
Ansandr <ansandr2005@gmail.com>
2023-12-01 20:34:01 +0200

    fix syntax error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 async function fetchRequest() {
   let password = document.getElementById("password").value;
   let url = "https://ansandr.pp.ua/quest23/login.php?password=" + password;
-  let response = await fetch(urll, {
+  let response = await fetch(url, {
     method: "GET",
     headers: {
       "Content-Type": "application/json;charset=UTF-8",
